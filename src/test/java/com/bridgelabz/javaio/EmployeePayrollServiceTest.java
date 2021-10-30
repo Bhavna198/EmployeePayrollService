f840474781ee54a00ba5a35ded7588c2f33e62c7
Bhavna Shende <bhavanashende661@gmail.com>
2021-10-30 17:19:14 +0530

    Method to print and count entries in file

---
+++ src/test/java/com/bridgelabz/javaio/EmployeePayrollServiceTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.javaio;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import com.bridgelabz.javaio.EmployeePayrollService.IOService;
+
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmployees = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assert.assertEquals(3, entries);
+
+    }
+}
+
